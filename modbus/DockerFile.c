b7704d1df0d80eb7da8754eb868dd836025cf092
Saifudheen <saifuhameed@gmail.com>
2025-12-14 08:50:40 +0300

    update

--- modbus/DockerFile.c
+++ modbus/DockerFile.c
@@ -1,6 +1,5 @@
-FROM gcc:latest AS build
+FROM arm32v7/debian:bullseye-slim
 
-WORKDIR /app
 
 # Install build tools and required libraries TEST
 RUN apt-get update && apt-get install -y \
@@ -26,8 +25,6 @@ COPY . .
 
 # Compile your C application
 RUN gcc -o  modbus_to_redis  modbus_to_redis.c config.c -lmodbus -lcjson -lsqlite3 -lhiredis
-
-FROM arm32v7/debian:bullseye-slim
 
 WORKDIR /app
 
